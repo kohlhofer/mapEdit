a2f644a3ff0ceeb88652001ad5a0a4e6a71c58f1
Alexander Kohlhofer <alexander@kohlhofer.com>
2013-05-08 13:57:21 -0400

    now creatig empty map object

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,32 @@
 function mapController($scope) {
-  alert('test');
+
+  $scope.setUpMap = function() {
+    
+    var width = 9;
+    var height = 9;
+    var map = new Array(8);
+    var defaultTerrain = false;
+    var defaultUnit = false;
+
+    for (var x = 0; x < width; x++) {
+      map[x] = new Array(height);
+      for (var y = 0; y < height; y++) {
+        map[x][y] = {
+          'terrain':defaultTerrain,
+          'unit':defaultUnit
+        }
+      }
+    }
+
+    return map;
+
+  }
+
+
+  $scope.map = $scope.setUpMap();
+
+
+
 }
 
 mapController.$inject = ['$scope'];
